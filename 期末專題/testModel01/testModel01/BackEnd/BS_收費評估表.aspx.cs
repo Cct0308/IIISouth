232c17e6bdea9455b9acb1524bca689636fae1a8
Cct0308 <dawnstarlet@hotmail.com>
2015-06-17 12:28:53 +0800

    20150617
    update2

---
+++ 期末專題/testModel01/testModel01/BackEnd/BS_收費評估表.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace testModel01
+{
+    public partial class WebForm11 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn上傳word檔_Click(object sender, EventArgs e)
+        {
+            string path = Server.MapPath("..");
+            FileUpload1.SaveAs(path + @"\AssessFile\assess.doc");
+        }
+
+        protected void btn上傳收費評估表_Click(object sender, EventArgs e)
+        {
+            string path = Server.MapPath("..");
+            FileUpload_收費評估表.SaveAs(path + @"\AssessFile\123.xlsx");
+        }                
+    }
+}
